14c4a1b6efec68c6ec64f330fa11797c82aa238d
CharefSarah <charef.sarah@gmx.fr>
2023-01-04 19:18:48 +0100

    push push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,82 @@
-gsap.registerPlugin(ScrollTrigger);
+function Marquee(selector, speed) {
+    const parentSelector = document.querySelector(selector);
+    const clone = parentSelector.innerHTML;
+    const firstElement = parentSelector.children[0];
+    let i = 0;
+    
+    parentSelector.insertAdjacentHTML('beforeend', clone);
+    parentSelector.insertAdjacentHTML('beforeend', clone);
+  
+    setInterval(function () {
+      firstElement.style.marginLeft = `-${i}px`;
+      if (i > firstElement.clientWidth) {
+        i = 0;
+      }
+      i = i + speed;
+    }, 0);
+  }
+  
 
-gsap.to(".chepo", {
-  scrollTrigger: {
-    trigger: ".projet",
-    markers: true,
-    start: "top top",
-    end: "bottom bottom",
-    pin: ".projet",
-    scrub: 2,
-  },
-  opacity: 1,
-  y: 500,
+  window.addEventListener('load', Marquee('.marquee', .2))
+
+
+
+  var btn = $('#button');
+
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
 });
+
+
+
+document.querySelector('#clickme').addEventListener('click',function(){
+  let mail = this.getAttribute('data-mail');
+  console.log(mail);
+  navigator.clipboard.writeText(mail);
+  this.innerHTML = "Mail copié  "
+})
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+  "#000",
+
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+  
+});
+
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
